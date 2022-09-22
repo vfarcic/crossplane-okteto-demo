5381d55a86a1e61eec47085daceb63a932088a26
Viktor Farcic <viktor@farcic.com>
2022-09-22 16:55:05 +0200

    Initial commit

---
+++ demo.sh
@@ -0,0 +1,39 @@
+#########
+# Intro #
+#########
+
+# TODO: Viktor: What is Crossplane?
+
+# TODO: Arsh: What is Okteto?
+
+#########
+# Setup #
+#########
+
+# TODO: Viktor: Create a cluster
+
+# TODO: Viktor: Apply Crossplane
+
+# TODO: Viktor: Apply Crossplane providers
+
+##########
+# Demo #
+##########
+
+# TODO: Viktor: Create Postgresql
+
+# TODO: Viktor: Create a schema in Postgresql
+
+# TODO: Viktor: Show how to retrieve DB authentication
+
+# TODO: Arsh: Use Oktetot to deploy an app connected to Postgresql
+
+# TODO: Arsh: Show that the apps is working and connected to the DB (e.g., create/retrieve some records in the DB)
+
+###########
+# Destroy #
+###########
+
+# TODO: Viktor: Destroy the DB
+
+# TODO: Viktor: Destroy the cluster
