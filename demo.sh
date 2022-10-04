5084646c63fa94fb30f9fde6a4bbf39b728d50ea
Viktor Farcic <viktor@farcic.com>
2022-10-04 16:47:29 +0200

    Update demo.sh

--- demo.sh
+++ demo.sh
@@ -2,15 +2,19 @@
 # Intro #
 #########
 
-# TODO: Viktor: What is Crossplane?
+# TODO: Viktor: What is Crossplane? (short, we'll explain more during and after the demo)
 
-# TODO: Arsh: What is Okteto?
+# TODO: Arsh: What is Okteto? (short, we'll explain more during and after the demo)
 
 #########
 # Setup #
 #########
 
 # Create a Kubernetes cluster (a local cluster like Rancher Desktop should do)
+
+# Install `okteto` CLI from https://www.okteto.com/docs/getting-started/#installing-okteto-cli
+
+git clone https://github.com/vfarcic/silly-demo
 
 git clone https://github.com/vfarcic/devops-toolkit-crossplane
 
@@ -74,6 +78,14 @@ kubectl --namespace a-team \
 
 # Wait until it is `READY`
 
+./examples/k8s/get-kubeconfig-eks.sh
+
+# TODO: Add the secret from the k8s composition
+kubectl --kubeconfig kubeconfig.yaml \
+    --namespace crossplane-system \
+    create secret generic aws-creds \
+    --from-file creds=./aws-creds.conf
+
 ########
 # Demo #
 ########
@@ -81,10 +93,10 @@ kubectl --namespace a-team \
 # Q: What would you like to do?
 # A: Simplify development
 
-# Q: How will you do that?
+# Q: How will you do that?
 # A: With Okteto
 
-# Q: What do you need?
+# Q: What do you need?
 # A: I need a cluster
 
 cat examples/k8s/aws-eks.yaml
@@ -95,80 +107,66 @@ kubectl --namespace a-team apply \
 kubectl --namespace a-team \
     get clusterclaims
 
+kubectl get managed
+
 # Q: What else do you need?
-# A: I need to be able to connect to that cluster
+# A: I need to be able to connect to that cluster
 
 kubectl --namespace a-team get secrets
 
 ./examples/k8s/get-kubeconfig-eks.sh
 
-kubectl --kubeconfig kubeconfig.yaml \
-    get nodes
+export KUBECONFIG=$PWD/kubeconfig.yaml
 
-# TODO: Add the secret from the k8s composition
-kubectl --kubeconfig kubeconfig.yaml \
-    --namespace crossplane-system \
-    create secret generic aws-creds \
-    --from-file creds=./aws-creds.conf
+kubectl get nodes
 
 # Q: What else do you need?
 # A: I need a shared database (I don't want to deploy it in my own environment)
 
-kubectl --kubeconfig kubeconfig.yaml \
-    --namespace production apply \
+kubectl --namespace production apply \
     --filename examples/sql/aws.yaml
 
 cat examples/sql/aws.yaml
 
-kubectl --kubeconfig kubeconfig.yaml \
-    --namespace production \
+kubectl --namespace production \
     get sqlclaims
 
 kubectl get managed
-
-kubectl --kubeconfig kubeconfig.yaml \
-    get managed
 
 cat packages/sql/definition.yaml
 
 cat packages/sql/aws.yaml
 
-kubectl --kubeconfig kubeconfig.yaml \
-    --namespace production \
+kubectl --namespace production \
     get sqlclaims
 
-# Talk about something until the claim is `READY`
+# Arsh: talk about Okteto if the claim is not yet ready
 
-# Q: What else do you need?
+# Q: What else do you need?
 # A: I need to be able to access that database server
 
-kubectl --kubeconfig kubeconfig.yaml \
-    --namespace production \
+kubectl --namespace production \
     get secrets
 
 export DB_ENDPOINT=$(kubectl \
-    --kubeconfig kubeconfig.yaml \
     --namespace production \
     get secret my-db \
     --output jsonpath='{.data.endpoint}' \
     | base64 --decode)
 
 export DB_PORT=$(kubectl \
-    --kubeconfig kubeconfig.yaml \
     --namespace production \
     get secret my-db \
     --output jsonpath='{.data.port}' \
     | base64 --decode)
 
 export DB_USER=$(kubectl \
-    --kubeconfig kubeconfig.yaml \
     --namespace production \
     get secret my-db \
     --output jsonpath='{.data.username}' \
     | base64 --decode)
 
 export DB_PASS=$(kubectl \
-    --kubeconfig kubeconfig.yaml \
     --namespace production \
     get secret my-db \
     --output jsonpath='{.data.password}' \
@@ -179,8 +177,7 @@ env | grep DB_
 # Q: What else do you need?
 # A: I need a database inside that DB server
 
-kubectl --kubeconfig kubeconfig.yaml \
-    get databases.postgresql.sql.crossplane.io
+kubectl get databases.postgresql.sql.crossplane.io
 
 # Open pgAdmin and show that the DB was created
 
@@ -191,29 +188,50 @@ cat examples/sql/schemahero-postgresql.yaml
 
 export DB_URI=postgresql://$DB_USER:$DB_PASS@$DB_ENDPOINT:$DB_PORT/my-db
 
-kubectl --kubeconfig kubeconfig.yaml \
-    --namespace production \
+kubectl --namespace production \
     create secret generic my-db-uri \
     --from-literal=value=$DB_URI
 
-kubectl --kubeconfig kubeconfig.yaml \
-    --namespace production \
+kubectl --namespace production \
     apply \
     --filename examples/sql/schemahero-postgresql.yaml
 
-kubectl --kubeconfig kubeconfig.yaml \
-    --namespace production \
+kubectl --namespace production \
     get databases.databases.schemahero.io
 
-kubectl --kubeconfig kubeconfig.yaml \
-    --namespace production \
+kubectl --namespace production \
     get tables.schemas.schemahero.io
 
-# TODO: Arsh: Use Okteto to deploy an app connected to Postgresql
+cd ../silly-demo
 
-# TODO: Arsh: The endpoint, port, user, and password are in the environment variables (`DB_*`).
+# TODO: Arsh: From here on it's all about Okteto. Choose to showcase whatever you think would be good to show.
 
-# TODO: Arsh: Show that the apps is working and connected to the DB (e.g., create/retrieve some records in the DB)
+# TODO: The commands that follow are there only to show you how to run the app. Change them to leverage Okteto.
+
+kubectl --namespace dev \
+    create secret generic my-db \
+    --from-literal=endpoint=$DB_ENDPOINT \
+    --from-literal=port=$DB_PORT \
+    --from-literal=username=$DB_USER \
+    --from-literal=password=$DB_PASS
+
+cat okteto.yml
+
+kubectl --namespace dev apply \
+    --kustomize kustomize/overlays/stateful
+
+kubectl --namespace dev port-forward \
+    svc/silly-demo 8080:8080 &
+
+curl -X POST "localhost:8080/video?id=wNBG1-PSYmE&title=Kubernetes%20Policies%20And%20Governance%20-%20Ask%20Me%20Anything%20With%20Jim%20Bugwadia"
+
+curl -X POST "localhost:8080/video?id=VlBiLFaSi7Y&title=Scaleway%20-%20Everything%20We%20Expect%20From%20A%20Cloud%20Computing%20Service%3F"
+
+curl "localhost:8080/videos" | jq .
+
+# TODO: Viktor: Arsh, what is Okteto?
+
+# TODO: Arsh: Viktor, what is Crossplane?
 
 ###########
 # Destroy #
@@ -243,4 +261,4 @@ kubectl get managed
 # Wait until all the resources are deleted
 #   (ignore `release` and `object resources`)
 
-# Destroy or reset the management cluster
+# Destroy or reset the management cluster
